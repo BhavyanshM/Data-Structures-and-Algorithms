26766d3eb85a3a6f3e66e531fda6753026d9a537
BhavyanshM <bhavyanshmishra@gmail.com>
2017-10-18 15:27:29 -0500

    Add the cityWeights sample file and the reader for the same in the grapher

--- TravellingSalesman/grapher.c
+++ TravellingSalesman/grapher.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(void){
+	int i = 0;
+	int N = 5;
+	char string[10];
+	FILE* fp = fopen("cityWeights.txt", "r");
+	if(fp!=NULL){
+		while(fscanf(fp, "%s\n", string)!=EOF){
+			printf("%s\n", string);
+		}
+	}else{
+		printf("The file was not found\n");
+	}
+	return 0;
+}
