af4c152b5bc70dd593b18b4ac1f80394085a6ca5
BhavyanshM <bhavyanshmishra@gmail.com>
2017-12-06 18:59:26 -0600

    Fixing the CRLF

--- DynamicProgramming/main.c
+++ DynamicProgramming/main.c
@@ -27,31 +27,12 @@ int main(){
 	lcs(X, Y, LCS);
 	printf("\n");
 
-<<<<<<< HEAD
-	int* a = malloc(sizeof(int)*6);
-	a[0] = 9;
-	a[1] = 3;
-	a[2] = 34;
-	a[3] = 5;
-	a[4] = 1;
-	a[5] = 65;
-	int* b = malloc(sizeof(int)*5);
-	b[0] = 1;
-	b[1] = 5;
-	b[2] = 2; 
-	b[3] = 9;
-	b[4] = 12;
 
-	// reset(a, 6, b, 5);	
-
-	// print(a, 5);
-	// print(b, 6);
-=======
 	FILE* msfp = fopen("multipleSequences.txt", "r");
 	int num = 0;
 	fscanf(msfp, "%d\n", &num);
 
-	display(num, msfp);	
+//	display(num, msfp);	
 	printf("\n\n");
 
 	int* starts = NULL;
@@ -62,10 +43,9 @@ int main(){
 
 	msfp = fopen("multipleSequences.txt", "r");
 	char str[2000];
-	fseek(msfp, 38  + OFFSET, SEEK_SET);
+	fseek(msfp, 40  + OFFSET, SEEK_SET);
 	fscanf(msfp, "%s\n", str);
 	printf("%s\n", str);
->>>>>>> 13486f5e74fb603a8cf5a727fb1828f9bde47843
 
 	return 0;
 }
--- DynamicProgramming/similarity.h
+++ DynamicProgramming/similarity.h
@@ -1,1 +1,4 @@
 char sim(int lcs, int a, int b);
+void display(int n, FILE* fp);
+void print(int* a, int n);
+void startings(int n, FILE* fp, int* s);
